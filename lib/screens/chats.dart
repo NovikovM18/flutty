3e7c57582f5f15c8c731764ed109d8ffb5edf32b
NovikovM18 <mnovikov.5prosoftware.com>
2023-04-15 11:58:29 +0300

    added Slidable pub

--- lib/screens/chats.dart
+++ lib/screens/chats.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:flutty/screens/chat.dart';
 
 class Chats extends StatefulWidget {
@@ -44,8 +45,34 @@ class _ChatsState extends State<Chats> {
       },
     );
   }
+
+  confirmDeleteChat() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Delete'),
+          content: const Text('Are you sure to delete this chat?'),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () => Navigator.of(context).pop(true),
+              child: const Text('DELETE')
+            ),
+            TextButton(
+              onPressed: () => Navigator.of(context).pop(false),
+              child: const Text('CANCEL'),
+            ),
+          ],
+        );
+      },
+    );
+  }
   
   Future addChat() async {
+
+  }
+  
+  Future deleteChat() async {
 
   }
 
@@ -73,61 +100,56 @@ class _ChatsState extends State<Chats> {
             itemCount: snapshot.data!.docs.length,
             itemBuilder: (context, index) {
               final chat = snapshot.data!.docs[index];
-              return ListTile(
-                leading: CircleAvatar(),
-                title: Row(
+              return Slidable(
+                key: Key(chat['id']),
+                endActionPane: ActionPane(
+                  // dismissible: DismissiblePane(onDismissed: () => showDialogs('Complited')), 
+                  motion: const StretchMotion(), 
                   children: [
-                    Expanded(
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(chat['name'], 
-                            style: Theme.of(context).textTheme.bodyMedium,
-                            maxLines: 1,
-                            overflow: TextOverflow.ellipsis,
-                          ),
-                        ],
-                      ),
-                    ),
-                ],),
-                subtitle: Row(
-                  children: [
-                    Text(chat['description'], 
-                      style:Theme.of(context).textTheme.bodySmall,
-                      maxLines: 1,
-                      overflow: TextOverflow.ellipsis,
+                    SlidableAction(
+                      onPressed: (context) => confirmDeleteChat(),
+                      backgroundColor: Colors.red,
+                      icon: Icons.delete_forever,
+                      label: 'Delete',
                     )
                   ],
                 ),
-                trailing: const Icon(Icons.arrow_forward_ios),
-                contentPadding: const EdgeInsets.fromLTRB(10, 10, 10, 10),
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => Chat(chatId: chat.id,)),
-                  );
-                },
-                onLongPress: () {
-                  showDialog(
-                    context: context,
-                    builder: (BuildContext context) {
-                      return AlertDialog(
-                        title: const Text('Delete'),
-                        content: const Text('Are you sure to delete this chat?'),
-                        actions: <Widget>[
-                          TextButton(
-                            onPressed: () => Navigator.of(context).pop(true),
-                            child: const Text('DELETE')
-                          ),
-                          TextButton(
-                            onPressed: () => Navigator.of(context).pop(false),
-                            child: const Text('CANCEL'),
-                          ),
-                        ],
-                      );
-                    },
-                  );
-                },
+                child: ListTile(
+                  leading: CircleAvatar(),
+                  title: Row(
+                    children: [
+                      Expanded(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(chat['name'], 
+                              style: Theme.of(context).textTheme.bodyMedium,
+                              maxLines: 1,
+                              overflow: TextOverflow.ellipsis,
+                            ),
+                          ],
+                        ),
+                      ),
+                  ],),
+                  subtitle: Row(
+                    children: [
+                      Text(chat['description'], 
+                        style:Theme.of(context).textTheme.bodySmall,
+                        maxLines: 1,
+                        overflow: TextOverflow.ellipsis,
+                      )
+                    ],
+                  ),
+                  trailing: const Icon(Icons.arrow_forward_ios),
+                  contentPadding: const EdgeInsets.fromLTRB(10, 10, 10, 10),
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => Chat(chatId: chat.id,)),
+                    );
+                  },
+                  onLongPress: () {},
+                ),
               );
             }, 
             separatorBuilder: (BuildContext context, int index) {
--- lib/screens/todos.dart
+++ lib/screens/todos.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutty/screens/todo.dart';
 import 'package:flutty/utils/vars.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 
 class ToDos extends StatefulWidget {
   const ToDos({Key? key}) : super(key: key);
@@ -34,6 +35,53 @@ class _ToDosState extends State<ToDos> {
     });
   }
 
+  showConfirmDialog(String type) {
+    switch (type) {
+      case 'Complited':
+        return showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return AlertDialog(
+              title: const Text('Confirm'),
+              content: const Text('Are you sure you wish to complete this item?'),
+              actions: <Widget>[
+                TextButton(
+                  onPressed: () => Navigator.of(context).pop(true),
+                  child: const Text('COMPLETE')
+                ),
+                TextButton(
+                  onPressed: () => Navigator.of(context).pop(false),
+                  child: const Text('CANCEL'),
+                ),
+              ],
+            );
+          },
+        );
+      case 'Processed':
+        return showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return AlertDialog(
+              title: const Text('Confirm'),
+              content: const Text('Are you sure you wish to process this item?'),
+              actions: <Widget>[
+                TextButton(
+                  onPressed: () => Navigator.of(context).pop(true),
+                  child: const Text('PROCESS')
+                ),
+                TextButton(
+                  onPressed: () => Navigator.of(context).pop(false),
+                  child: const Text('CANCEL'),
+                ),
+              ],
+            );
+          },
+        );
+      default:
+      break;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -55,69 +103,27 @@ class _ToDosState extends State<ToDos> {
             itemCount: snapshot.data!.docs.length,
             itemBuilder: (context, index) {
               final todo = snapshot.data!.docs[index];
-              return Dismissible(
+              return Slidable(
                 key: Key(todo['id']),
-                dismissThresholds: <DismissDirection, double>{},
-                confirmDismiss: (DismissDirection direction) async {
-                  switch (direction) {
-                    case DismissDirection.startToEnd:
-                      return await showDialog(
-                        context: context,
-                        builder: (BuildContext context) {
-                          return AlertDialog(
-                            title: const Text('Confirm'),
-                            content: const Text('Are you sure you wish to complete this item?'),
-                            actions: <Widget>[
-                              TextButton(
-                                onPressed: () => Navigator.of(context).pop(true),
-                                child: const Text('COMPLETE')
-                              ),
-                              TextButton(
-                                onPressed: () => Navigator.of(context).pop(false),
-                                child: const Text('CANCEL'),
-                              ),
-                            ],
-                          );
-                        },
-                      );
-                    case DismissDirection.endToStart:
-                      return await showDialog(
-                        context: context,
-                        builder: (BuildContext context) {
-                          return AlertDialog(
-                            title: const Text('Confirm'),
-                            content: const Text('Are you sure you wish to delete this item?'),
-                            actions: <Widget>[
-                              TextButton(
-                                onPressed: () => Navigator.of(context).pop(true),
-                                child: const Text('DELETE')
-                              ),
-                              TextButton(
-                                onPressed: () => Navigator.of(context).pop(false),
-                                child: const Text('CANCEL'),
-                              ),
-                            ],
-                          );
-                        },
-                      );
-                    default:
-                      break;
-                  }
-                },
-                background: 
-                  Container(
-                    padding: const EdgeInsets.fromLTRB(20, 0, 0, 0),
-                    alignment: Alignment.centerLeft,
-                    color: Colors.green,
-                    child: const Icon(Icons.check, color: Colors.white),
-                  ),
-                secondaryBackground: 
-                  Container(
-                    padding: const EdgeInsets.fromLTRB(0, 0, 20, 0),
-                    alignment: Alignment.centerRight,
-                    color: Colors.red,
-                    child: const Icon(Icons.delete_forever, color: Colors.white),
-                  ),
+                endActionPane: ActionPane(
+                  // dismissible: DismissiblePane(onDismissed: () => showConfirmDialog('Complited')), 
+                  motion: const StretchMotion(), 
+                  children: [
+                    !todo['complited']
+                    ? SlidableAction(
+                        onPressed: (context) => showConfirmDialog('Complited'),
+                        backgroundColor: Colors.green,
+                        icon: Icons.access_alarm_sharp,
+                        label: 'Complited',
+                      )
+                    :  SlidableAction(
+                        onPressed: (context) => showConfirmDialog('Processed'),
+                        backgroundColor: Colors.yellow,
+                        icon: Icons.access_alarm_sharp,
+                        label: 'Processed',
+                      )
+                  ],
+                ),
                 child: ListTile(                  
                   title: Row(
                     children: [
@@ -156,6 +162,107 @@ class _ToDosState extends State<ToDos> {
                   },
                 ),
               );
+              // return Dismissible(
+              //   key: Key(todo['id']),
+              //   dismissThresholds: <DismissDirection, double>{},
+              //   confirmDismiss: (DismissDirection direction) async {
+              //     switch (direction) {
+              //       case DismissDirection.startToEnd:
+              //         return await showDialog(
+              //           context: context,
+              //           builder: (BuildContext context) {
+              //             return AlertDialog(
+              //               title: const Text('Confirm'),
+              //               content: const Text('Are you sure you wish to complete this item?'),
+              //               actions: <Widget>[
+              //                 TextButton(
+              //                   onPressed: () => Navigator.of(context).pop(true),
+              //                   child: const Text('COMPLETE')
+              //                 ),
+              //                 TextButton(
+              //                   onPressed: () => Navigator.of(context).pop(false),
+              //                   child: const Text('CANCEL'),
+              //                 ),
+              //               ],
+              //             );
+              //           },
+              //         );
+              //       case DismissDirection.endToStart:
+              //         return await showDialog(
+              //           context: context,
+              //           builder: (BuildContext context) {
+              //             return AlertDialog(
+              //               title: const Text('Confirm'),
+              //               content: const Text('Are you sure you wish to delete this item?'),
+              //               actions: <Widget>[
+              //                 TextButton(
+              //                   onPressed: () => Navigator.of(context).pop(true),
+              //                   child: const Text('DELETE')
+              //                 ),
+              //                 TextButton(
+              //                   onPressed: () => Navigator.of(context).pop(false),
+              //                   child: const Text('CANCEL'),
+              //                 ),
+              //               ],
+              //             );
+              //           },
+              //         );
+              //       default:
+              //         break;
+              //     }
+              //   },
+              //   background: 
+              //     Container(
+              //       padding: const EdgeInsets.fromLTRB(20, 0, 0, 0),
+              //       alignment: Alignment.centerLeft,
+              //       color: Colors.green,
+              //       child: const Icon(Icons.check, color: Colors.white),
+              //     ),
+              //   secondaryBackground: 
+              //     Container(
+              //       padding: const EdgeInsets.fromLTRB(0, 0, 20, 0),
+              //       alignment: Alignment.centerRight,
+              //       color: Colors.red,
+              //       child: const Icon(Icons.delete_forever, color: Colors.white),
+              //     ),
+              //   child: ListTile(                  
+              //     title: Row(
+              //       children: [
+              //         if (selectedTab == 0) 
+              //         todo['complited'] 
+              //           ? const Icon(Icons.done_all, color: customColors.green,) 
+              //           : const Icon(Icons.work, color: customColors.yellow,),
+              //         const SizedBox(width: 20),
+              //         Expanded(
+              //           child: Column(
+              //             crossAxisAlignment: CrossAxisAlignment.start,
+              //             children: [
+              //               Text(todo['name'], 
+              //                 style: Theme.of(context).textTheme.bodyMedium,
+              //                 maxLines: 1,
+              //                 overflow: TextOverflow.ellipsis,
+              //               ),
+              //               Text(todo['description'],
+              //                 style: Theme.of(context).textTheme.bodySmall,
+              //                 maxLines: 2,
+              //                 overflow: TextOverflow.ellipsis,
+              //               ),
+              //             ],
+              //           ),
+              //         ),
+              //       ],
+              //     ),
+              //     trailing: const Icon(Icons.arrow_forward_ios),
+              //     contentPadding: const EdgeInsets.fromLTRB(10, 10, 10, 10),
+              //     onTap: () {
+              //       selectedTodo = todo;
+              //       Navigator.push(
+              //         context,
+              //         MaterialPageRoute(builder: (context) => ToDo(selectedTodo: selectedTodo)),
+              //       );
+              //     },
+              //   ),
+              // );
             }, 
             separatorBuilder: (BuildContext context, int index) {
               return const SizedBox(height: 4);
